f431a95a4779d6f14e803be39880ae854c00866b
Chris Melin <Chris.Melin@cristie.se>
2019-08-07 14:18:33 +0200

    Testing

---
+++ hangman.py
@@ -0,0 +1,21 @@
+answer = "fiskbulle"
+tries = 10
+disp = ''
+for i in answer:
+    disp = disp + "_"
+
+display = list(disp)
+
+while tries > 0:
+    guess = input("Guess a letter: ")
+    if guess in answer:
+        print ("Yes, " + guess + " is in the word")
+        loc = [pos for pos, char in enumerate(answer) if char == guess]
+        for num in loc:
+            display[num] = guess
+        print (display)
+    else:
+        print ("No, there is no " + guess + " in the word")
+        tries = tries -1
+        print ("You have " + str(tries) + " tries left") 
+
